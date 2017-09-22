418f22e8d0d1c1e282cf87a04f4272146685071f
ticky74 <ikulmatycki@gmail.com>
2017-09-22 11:52:25 -0600

    Adding test client

---
+++ tests/TestClient/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
